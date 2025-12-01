e28a74d64277c3a7467ef37ec86dcb74c9e05dca
marwamedhat95 <marwa.srk95@gmail.com>
2025-12-01 16:38:04 +0200

    edit style home page

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -1,13 +1,14 @@
-// backeend/config/cloudinary.js
-import { v2 as cloudinary } from 'cloudinary';
-import dotenv from 'dotenv';
+// backend/config/cloudinary.js
+
+const cloudinary = require('cloudinary').v2;
+const dotenv = require('dotenv');
 
 dotenv.config();
 
-cloudinary.config({ 
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
-  api_key: process.env.CLOUDINARY_API_KEY, 
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET
 });
 
-export default cloudinary;
+module.exports = cloudinary;
